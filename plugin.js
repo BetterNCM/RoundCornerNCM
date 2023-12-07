3cdc79f1eae1890dfbed3e81c2c01a111872a0b3
Vincent1230 <516879257@qq.com>
2023-12-08 03:38:14 +0800

    update plugin.js - 修复最大化窗口时与多屏幕冲突的问题 (在最大化时取消圆角效果 https://github.com/BetterNCM/RoundCornerNCM/issues/4) - 修复最大化窗口时与透明任务栏冲突的问题 (最大化下边框有虚线 https://github.com/BetterNCM/RoundCornerNCM/issues/5)

--- plugin.js
+++ plugin.js
@@ -1,1 +1,16 @@
-betterncm.app.setRoundedCorner(true)
+function isWindowMaximized() {
+    const isWidthMaximized = window.outerWidth === window.screen.availWidth;
+    const isHeightMaximized = window.outerHeight === window.screen.availHeight;
+
+    return isWidthMaximized && isHeightMaximized;
+}
+
+function checkWindowMaximized() {
+    const isMaximized = isWindowMaximized();
+
+    betterncm.app.setRoundedCorner(!isMaximized);
+}
+
+window.addEventListener('resize', checkWindowMaximized);
+
+checkWindowMaximized();
